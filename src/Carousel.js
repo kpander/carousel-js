2c5aac32adb405d4933d8f8f49dc588b07e38905
Kendall Anderson <kpander@invisiblethreads.com>
2023-08-15 15:32:30 -0400

    Converts carousel to native web component
    This is a breaking change. The carousel is now a native web
    component, `<carousel-ithreads>`.

--- src/Carousel.js
+++ src/Carousel.js
@@ -4,66 +4,108 @@
  * Carousel.js
  */
 
-class Carousel {
-
+class CarouselIthreads extends HTMLElement {
   constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+
+    // https://stackoverflow.com/questions/68475272/when-can-we-access-the-children-elements-of-a-custom-component-using-javascript
+    this.onMutation = this.onMutation.bind(this);
+
+    const template = document.createElement("template");
+    template.innerHTML = `
+<style>
+:host {
+  display: block;
+}
+</style>
+<div id="container">
+  <slot></slot>
+</div>
+<button id="btnPrev">Previous</button>
+<button id="btnNext">Next</button>
+`;
+    const clone = template.content.cloneNode(true);
+    this.shadowRoot.appendChild(clone);
+  }
+
+  connectedCallback() {
+    this.observer = new MutationObserver(this.onMutation);
+    this.observer.observe(this, {
+      childList: true
+    });
+
     this.state = {
-      active: 0,
+      current: 0,
       total: 0,
     };
 
-    this._countItems();
-    this.activate(this.state.active);
-    this.bindButtons();
+    this.shadowRoot.getElementById("btnPrev").addEventListener("click", this.previous.bind(this));
+    this.shadowRoot.getElementById("btnNext").addEventListener("click", this.next.bind(this));
   }
 
-  _countItems() {
-    const selector = "[data-csjs-container] [data-csjs-item]";
-    const els = document.querySelectorAll(selector);
-    els.forEach((el, index) => {
-      el.setAttribute("data-csjs-item-id", index);
-      el.setAttribute("hidden", "hidden");
-    });
-    this.state.total = els.length;
+  disconnectedCallback() {
+    this.observer.disconnect();
   }
 
-  activate(index) {
-    if (index > this.state.total - 1) index = 0;
-    if (index < 0) index = this.state.total - 1;
+  onMutation(mutations) {
+    const added = [];
 
-    // Hide current item.
-    const selectorOld = `[data-csjs-item-id="${this.state.active}"]`;
-    document.querySelector(selectorOld).setAttribute("hidden", "hidden");
+    // A `mutation` is passed for each new node
+    for (const mutation of mutations) {
+      // Could test for `mutation.type` here, but since we only have
+      // set up one observer type it will always be `childList`
+      added.push(...mutation.addedNodes);
+    }
 
-    // Show new item.
-    this.state.active = index;
-    const selectorNew = `[data-csjs-item-id="${this.state.active}"]`;
-    document.querySelector(selectorNew).removeAttribute("hidden");
-
-    return this.state.active;
+    this.init();
   }
 
-  next() {
-    return this.activate(this.state.active + 1);
+  init() {
+    this.state.total = [...this.childNodes].filter(el => {
+      return el.nodeType === Node.ELEMENT_NODE;
+    }).length;
+
+    this.activate(this.state.current);
   }
 
+  /**
+   * Show the previous slide.
+   */
   previous() {
-    return this.activate(this.state.active - 1);
+    return this.activate(this.state.current - 1);
   }
 
-  bindButtons() {
-    const me = this;
-    document.querySelectorAll("[data-csjs-previous]").forEach(el => {
-      el.addEventListener("click", me.previous.bind(this));
-    });
-    document.querySelectorAll("[data-csjs-next]").forEach(el => {
-      el.addEventListener("click", me.next.bind(this));
-    });
+  /**
+   * Show the next slide.
+   */
+  next() {
+    return this.activate(this.state.current + 1);
   }
+
+  /**
+   * Display the item at the given index.
+   *
+   * If the index is less or more than the allowable range, it will
+   * be reset to something valid.
+   */
+  activate(newIndex) {
+    if (newIndex < 0) newIndex = this.state.total - 1;
+    if (newIndex >= this.state.total) newIndex = 0;
+    this.state.current = newIndex;
+
+    [...this.childNodes].filter(el => {
+      return el.nodeType === Node.ELEMENT_NODE;
+    }).forEach((el, index) => {
+      if (index === newIndex) {
+        el.removeAttribute("hidden");
+      } else {
+        el.setAttribute("hidden", "hidden");
+      }
+    });
+
+    return this.state.current;
+  }
+
 }
-
-const carousel_init = function() {
-  window.carousel = new Carousel();
-};
-
-document.addEventListener("DOMContentLoaded", carousel_init);
+customElements.define('carousel-ithreads', CarouselIthreads);
